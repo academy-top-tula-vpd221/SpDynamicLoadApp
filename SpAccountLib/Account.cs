ac6ed55fd53a778e9993e7d93e5ed49d02d26c89
Maxim Efimov <efimov_m@t.top-academy.ru>
2023-11-19 14:03:23 +0300

    Добавьте файлы проекта.

---
+++ SpAccountLib/Account.cs
@@ -0,0 +1,31 @@
+﻿namespace SpAccountLib
+{
+    public class Account
+    {
+        public string? Number { get; set; }
+        public int Rub { set; get; }
+        public int Kop { set; get; }
+
+        public Account() : this("000", 0, 0) { }
+
+        public Account(string? number) : this(number, 0, 0) { }
+
+        public Account(string? number, int rub, int kop)
+        {
+            Number = number;
+            Rub = rub;
+            Kop = kop;
+        }
+
+        public void Add(int Rub, int Kop)
+        {
+            this.Rub += Rub;
+            this.Kop += Kop;
+        }
+
+        public override string ToString()
+        {
+            return $"Number {Number}. {Rub} Rub, {Kop} Kop";
+        }
+    }
+}
---
+++ SpDynamicLoadApp/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Reflection;
+
+/*
+Assembly assembly = Assembly.LoadFrom("SpAccountLib.dll");
+
+var type = assembly.GetType("SpAccountLib.Account");
+var account1 = Activator.CreateInstance(type, new object[] { "12345" });
+var account2 = Activator.CreateInstance(type, new object[] { "12345", 45, 87 });
+var account0 = Activator.CreateInstance(type, new object[] { });
+
+Console.WriteLine(account1);
+Console.WriteLine(account2);
+Console.WriteLine(account0);
+
+var types = assembly.GetTypes();
+*/
+
+//var n = 100;
+
+/*
+dynamic dobj = "Hello";
+Console.WriteLine(dobj.GetType());
+dobj = 10;
+Console.WriteLine(dobj.GetType());
+dobj += 100;
+
+
+object oobj = "Hello";
+Console.WriteLine(oobj.GetType());
+oobj = 10;
+Console.WriteLine(oobj.GetType());
+//oobj += 100;
+
+*/
+
+Employee bob = new Employee("Bob", 22);
+Employee joe = new Employee("Joe", "Twenty Two");
+
+dynamic sam = new Employee("Sam", 42);
+dynamic leo = new Employee("Leo", "Fourty Two");
+
+Console.WriteLine(bob);
+Console.WriteLine(joe);
+Console.WriteLine(sam);
+Console.WriteLine(leo);
+
+var s1 = bob.GetSalary(15000, "int");
+Console.WriteLine($"{s1.GetType()} {s1}");
+var s2 = joe.GetSalary(35000, "string");
+Console.WriteLine($"{s2.GetType()} {s2}");
+
+
+class Employee
+{
+    public string Name { get; set; }
+    public dynamic Age { get; set; }
+    public Employee(string name, dynamic age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public dynamic GetSalary(dynamic value, string format)
+    {
+        if (format == "string")
+            return $"{value} rub";
+        else if (format == "int")
+            return value;
+        else
+            return 0.0;
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {Name} Age: {Age}";
+    }
+}
